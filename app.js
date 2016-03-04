807d471e89b6b6c60498b7a6fd640d0bd5232d93
CarlosX <carlosx_1302@hotmail.com>
2016-03-04 18:02:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 alert('hi');
+
+
+ if (CodeMirror !== 'undefined') {
+   the.editor = CodeMirror.fromTextArea(textArea, {
+     theme: 'default',
+     lineNumbers: true
+   });
+   the.editor.focus();
+   the.editor.setValue(default_text);
+   $('.CodeMirror').click(function () {
+     if (the.editor.getValue() == default_text) {
+       the.editor.setValue('');
+     }
+   });
+ } 
