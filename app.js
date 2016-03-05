bca7ec94919c1d7160c51f8560520322bde05a7e
CarlosX <carlosx_1302@hotmail.com>
2016-03-05 10:16:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 var default_text ="// This is just a sample script. Paste your real code (javascript or HTML) here.\n\nif ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}";
 var textArea = $('#source')[0];
+var editor = null;
 
  if (CodeMirror !== 'undefined') {
-   the.editor = CodeMirror.fromTextArea(textArea, {
+   editor = CodeMirror.fromTextArea(textArea, {
      theme: 'default',
      lineNumbers: true
    });
-   the.editor.focus();
-   the.editor.setValue(default_text);
+   editor.focus();
+   editor.setValue(default_text);
    $('.CodeMirror').click(function () {
-     if (the.editor.getValue() == default_text) {
-       the.editor.setValue('');
+     if (editor.getValue() == default_text) {
+       editor.setValue('');
      }
    });
  } 
